2133d6b206adfa866df8767960558edd7ab86c33
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-02-15 12:01:59 +0500

    jsut adding UI for about us and contact us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 
 import Home from './Pages/Home'
+import AboutUs from './Pages/AboutUs'
+import ContactUs from './Pages/ContactUs'
 import "./App.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,38 +2,52 @@ import { MdWhatsapp } from "react-icons/md";
 import { useState } from "react";
 import { BsArrowUpRight } from "react-icons/bs";
 import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
   const [MenuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
   return (
     <nav className="flex  items-center justify-between px-8 py-6 max-w-7xl mx-auto">
       <div className="flex items-center gap-2 text-2xl font-bold tracking-tighter">
-        <img
-          src="./HMKCodeWebLogo.png"
-          className="h-15 cursor-pointer w-30"
-          alt="HMK CodeWeb Logo"
-          title="HMK CodeWeb Logo"
-        />
+        <Link to={"/"}>
+          <img
+            src="./HMKCodeWebLogo.png"
+            className="h-15 cursor-pointer w-30"
+            alt="HMK CodeWeb Logo"
+            title="HMK CodeWeb Logo"
+          />
+        </Link>
       </div>
       <div className="hidden md:flex gap-12 text-md font-medium text-gray-300">
-        <a href="#Footer" className="hover:text-[#07BEC8] font-semibold">
+        <Link to="/about" className="hover:text-[#07BEC8] font-semibold">
           About us
-        </a>
-        <a href="#Services" className="hover:text-[#07BEC8] font-semibold">
-          What We Do
-        </a>
-        <a href="#Clients" className="hover:text-[#07BEC8] font-semibold">
-          Our Results
-        </a>
-        <a href="#Projects" className="hover:text-[#07BEC8] font-semibold">
-          Insights
-        </a>
-        <a href="#Faqs" className="hover:text-[#07BEC8] font-semibold">
+        </Link>
+        {location.pathname === "/about" || location.pathname === "/contact" ? (
+          ""
+        ) : (
+          <>
+            <a href="#Services" className="hover:text-[#07BEC8] font-semibold">
+              What We Do
+            </a>
+            <a href="#Clients" className="hover:text-[#07BEC8] font-semibold">
+              Our Results
+            </a>
+            <a href="#Projects" className="hover:text-[#07BEC8] font-semibold">
+              Insights
+            </a>
+          </>
+        )}
+        <Link to="/contact" className="hover:text-[#07BEC8] font-semibold">
           Contact
-        </a>
+        </Link>
       </div>
       <button
-        onClick={() => window.open("https://wa.me/923437117831?text=Hello%20HMK%20CodeWeb!%20I%20would%20like%20to%20inquire%20about%20your%20services.%20%20Am%20Here%20To%20Discuss%20My%20Project%20And%20How%20You%20Can%20Help%20Me.%20Looking%20Forward%20To%20Hearing%20From%20You!")}
+        onClick={() =>
+          window.open(
+            "https://wa.me/923437117831?text=Hello%20HMK%20CodeWeb!%20I%20would%20like%20to%20inquire%20about%20your%20services.%20%20Am%20Here%20To%20Discuss%20My%20Project%20And%20How%20You%20Can%20Help%20Me.%20Looking%20Forward%20To%20Hearing%20From%20You!",
+          )
+        }
         className="relative hidden md:flex cursor-pointer bg-[#f3e3d3] text-black px-4 py-1 rounded-full group  items-center gap-2 font-bold hover:bg-white transition-colors"
       >
         Contact Us
@@ -51,21 +65,37 @@ const NavBar = () => {
       {MenuOpen ? (
         <div className="flex flex-col gap-5 right-0 px-10 py-20 z-20 absolute bg-[#000000de] top-20 w-full h-full">
           <div className="flex flex-col gap-6 text-sm font-medium text-gray-300">
-            <a href="#Footer" className="hover:text-[#07BEC8] font-semibold">
+            <Link to="/about" className="hover:text-[#07BEC8] font-semibold">
               About us
-            </a>
-            <a href="#Services" className="hover:text-[#07BEC8] font-semibold">
-              What We Do
-            </a>
-            <a href="#Clients" className="hover:text-[#07BEC8] font-semibold">
-              Our Results
-            </a>
-            <a href="#Projects" className="hover:text-[#07BEC8] font-semibold">
-              Insights
-            </a>
-            <a href="#Faqs" className="hover:text-[#07BEC8] font-semibold">
+            </Link>
+            {location.pathname === "/about" ||
+            location.pathname === "/contact" ? (
+              ""
+            ) : (
+              <>
+                <a
+                  href="#Services"
+                  className="hover:text-[#07BEC8] font-semibold"
+                >
+                  What We Do
+                </a>
+                <a
+                  href="#Clients"
+                  className="hover:text-[#07BEC8] font-semibold"
+                >
+                  Our Results
+                </a>
+                <a
+                  href="#Projects"
+                  className="hover:text-[#07BEC8] font-semibold"
+                >
+                  Insights
+                </a>
+              </>
+            )}
+            <Link to="/contact" className="hover:text-[#07BEC8] font-semibold">
               Contact
-            </a>
+            </Link>
           </div>
           <button
             onClick={() => window.open("wa.me/923437117831")}
