97b2b24cab34fbf3b53ce2dec6d5c45f0adb9a7a
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-02-16 18:41:31 +0500

    about us complete

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -2,7 +2,7 @@ import NavBar from "../Components/NavBar";
 
 export default function AboutUs() {
   return (
-    <>
+    <div className="w-full pb-10 min-h-screen bg-[#0B0B0B] ">
       <style>{`
                 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
             
@@ -11,14 +11,20 @@ export default function AboutUs() {
                 }
             `}</style>
       <NavBar />
-      <section className="flex flex-col md:flex-row items-center justify-center gap-10 max-md:px-4">
+      <section className="flex flex-col md:flex-row w-full h-full items-center justify-center gap-10 max-md:px-4">
         <div className="relative shadow-2xl shadow-indigo-600/40 rounded-2xl overflow-hidden shrink-0">
           <img
             className="max-w-md w-full object-cover rounded-2xl"
             src="https://images.unsplash.com/photo-1531497865144-0464ef8fb9a9?q=80&w=451&h=451&auto=format&fit=crop"
             alt=""
           />
-          <div className="flex items-center gap-1 max-w-72 absolute bottom-8 left-8 bg-white p-4 rounded-xl">
+          <div
+            onClick={() =>
+              window.open("https://chat.whatsapp.com/JhqCwKwV0B5LZrcPfWOSsM")
+            }
+            title="On WhatsApp"
+            className="flex select-none items-center gap-1 max-w-72 absolute bottom-8 left-8 bg-white hover:scale-95 duration-200 cursor-pointer active:scale-105 p-4 rounded-xl"
+          >
             <div className="flex -space-x-4 shrink-0">
               <img
                 src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200"
@@ -36,7 +42,7 @@ export default function AboutUs() {
                 className="size-9 rounded-full border-[3px] border-white hover:-translate-y-1 transition z-[3]"
               />
               <div className="flex items-center justify-center text-xs  text-white size-9 rounded-full border-[3px] border-white bg-indigo-600 hover:-translate-y-1 transition z-[4]">
-                50+
+                90+
               </div>
             </div>
             <p className="text-sm font-medium text-slate-800">
@@ -48,40 +54,42 @@ export default function AboutUs() {
           <h1 className="text-xl uppercase font-semibold text-slate-700">
             What we do?
           </h1>
-          <div className="w-24 h-[3px] rounded-full bg-gradient-to-r from-indigo-600 to-[#DDD9FF]"></div>
-          <p className="mt-8">
-            PrebuiltUI helps you build faster by transforming your design vision
-            into fully functional, production-ready UI components.{" "}
-          </p>
-          <p className="mt-4">
-            Whether you're launching a SaaS app, landing page, or dashboard, our
-            collection of Tailwind CSS components is crafted to boost your
-            development speed and improve user experience.
-          </p>
-          <p className="mt-4">
-            From UI design systems to automation-ready layouts, PrebuiltUI
-            empowers you to build beautifully and scale effortlessly.
-          </p>
-          <a
-            href="#"
-            className="flex items-center w-max gap-2 mt-8 hover:-translate-y-0.5 transition bg-gradient-to-r from-indigo-600 to-[#8A7DFF] py-3 px-8 rounded-full text-white"
-          >
-            <span>Read more</span>
-            <svg
-              width="13"
-              height="12"
-              viewBox="0 0 13 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12.53 6.53a.75.75 0 0 0 0-1.06L7.757.697a.75.75 0 1 0-1.06 1.06L10.939 6l-4.242 4.243a.75.75 0 0 0 1.06 1.06zM0 6v.75h12v-1.5H0z"
-                fill="#fff"
-              />
-            </svg>
-          </a>
+          <div className="w-24  h-[3px] rounded-full bg-gradient-to-r from-indigo-600 to-[#DDD9FF]"></div>
+          <p className="mt-3">
+            We are a results-driven development team focused on building modern,
+            scalable digital solutions using the MERN stack and intelligent AI
+            integrations. Our mission is simple: to turn ideas into reliable,
+            high-performance web applications that help businesses grow and
+            operate more efficiently.
+          </p>{" "}
+          <p className="mt-3">
+            Over time, we’ve worked with clients across different industries,
+            delivering secure payment integrations, complete backend systems,
+            and full-scale web platforms. From integrating international payment
+            gateways like bKash to building fully functional admin dashboards
+            for eCommerce businesses, we focus on writing clean, maintainable
+            code that performs under real-world conditions. We don’t just build
+            features. We build systems that make business operations smoother
+            and smarter.
+          </p>{" "}
+          <p className="mt-3">
+            Our experience also extends to education platforms where we’ve
+            developed structured systems that manage student data, track
+            progress, verify certificates publicly, and give administrators
+            complete control over courses and enrollments. Every project we take
+            on is approached with clarity, careful planning, and attention to
+            long-term scalability.
+          </p>{" "}
+          <p className="mt-3">
+            {" "}
+            What sets us apart is our practical mindset. We understand business
+            logic, user experience, and technical architecture equally well.
+            Whether it’s backend engineering, cloud deployment, or AI-powered
+            automation, we aim to deliver solutions that are not only functional
+            but future-ready.
+          </p>{" "}
         </div>
       </section>
-    </>
+    </div>
   );
 }
